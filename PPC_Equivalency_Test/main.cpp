329918f7fd6933bd153b068f4a582f978986cb21
Phil Smith <philsmith@Phils-MacBook-Pro.local>
2018-01-29 12:48:09 +0000

    Initial Commit

---
+++ PPC_Equivalency_Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PPC_Equivalency_Test
+//
+//  Created by Phil Smith on 29/01/2018.
+//  Copyright © 2018 Phil Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
